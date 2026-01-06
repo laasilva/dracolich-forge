260f1c0bc7b6731adc004f5bfcfdaed86d84ae4e
Larissa Silva <cmdrlias@gmail.com>
2026-01-06 16:59:13 -0300

    update: adding component scans

---
+++ roller/src/main/java/dm/dracolich/forge/config/ForgeAutoConfiguration.java
@@ -0,0 +1,9 @@
+package dm.dracolich.forge.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "dm.dracolich.forge")
+public class ForgeAutoConfiguration {
+}
